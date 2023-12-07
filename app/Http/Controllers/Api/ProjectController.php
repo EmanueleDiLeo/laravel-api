c360c21acc6601e518a55d0eb670e3da54870a04
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-12-07 16:59:12 +0100

    [ADD] : slug route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,8 +9,14 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::all();
+        // $projects = Project::all();
+        $projects = Project::paginate(12);
         // $projects = Project::with('type')->all();
         return response()->json($projects);
+    }
+
+    public function getProjectBySlug($slug){
+        $project = Project::where('slug',$slug)->first();
+        return response()->json($project);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use App\Http\Controllers\Api\ProjectController;
 */
 
 Route::get('/projects',[ProjectController::class, 'index']);
+Route::get('/projects/get-project/{slug}',[ProjectController::class, 'getProjectBySlug']);
